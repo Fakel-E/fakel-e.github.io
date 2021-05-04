e7fec02d025cc99f622b85036a61ca8e55753613
Evgeniy <53363514+Fakel-E@users.noreply.github.com>
2021-05-04 16:36:32 +0300

    Добавляет страницу календарь.

--- js/calendar.js
+++ js/calendar.js
@@ -1,40 +1,63 @@
-function calendar(id, year, month) {
-    var Dlast = new Date(year,month+1,0).getDate(),
-        D = new Date(year,month,Dlast),
-        DNlast = new Date(D.getFullYear(),D.getMonth(),Dlast).getDay(),
-        DNfirst = new Date(D.getFullYear(),D.getMonth(),1).getDay(),
-        calendar = '<tr>',
-        month=["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"];
-    if (DNfirst != 0) {
-      for(var  i = 1; i < DNfirst; i++) calendar += '<td>';
-    }else{
-      for(var  i = 0; i < 6; i++) calendar += '<td>';
-    }
-    for(var  i = 1; i <= Dlast; i++) {
-      if (i == new Date().getDate() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
-        calendar += '<td class="today">' + i;
-      }else{
-        calendar += '<td>' + i;
-      }
-      if (new Date(D.getFullYear(),D.getMonth(),i).getDay() == 0) {
-        calendar += '<tr>';
-      }
-    }
-    for(var  i = DNlast; i < 7; i++) calendar += '<td> ';
-    document.querySelector('#'+id+' tbody').innerHTML = calendar;
-    document.querySelector('#'+id+' thead td:nth-child(2)').innerHTML = month[D.getMonth()] +' '+ D.getFullYear();
-    document.querySelector('#'+id+' thead td:nth-child(2)').dataset.month = D.getMonth();
-    document.querySelector('#'+id+' thead td:nth-child(2)').dataset.year = D.getFullYear();
-    if (document.querySelectorAll('#'+id+' tbody tr').length < 6) {  // чтобы при перелистывании месяцев не "подпрыгивала" вся страница, добавляется ряд пустых клеток. Итог: всегда 6 строк для цифр
-        document.querySelector('#'+id+' tbody').innerHTML += '<tr><td> <td> <td> <td> <td> <td> <td> ';
-    }
-    }
-    calendar("calendar", new Date().getFullYear(), new Date().getMonth());
-    // переключатель минус месяц
-    document.querySelector('#calendar thead tr:nth-child(1) td:nth-child(1)').onclick = function() {
-      calendar("calendar", document.querySelector('#calendar thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar thead td:nth-child(2)').dataset.month)-1);
-    }
-    // переключатель плюс месяц
-    document.querySelector('#calendar thead tr:nth-child(1) td:nth-child(3)').onclick = function() {
-      calendar("calendar", document.querySelector('#calendar thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar thead td:nth-child(2)').dataset.month)+1);
+'use strict'
+
+const leader = document.querySelector('.record-list__wrap--leader');
+const finance = document.querySelector('.record-list__wrap--finance');
+const desing = document.querySelector('.record-list__wrap--desing-think');
+const system = document.querySelector('.record-list__wrap--system-think');
+const team = document.querySelector('.record-list__wrap--team');
+
+const leaderFrame = document.querySelector('.form__frame--leader');
+const financeFrame = document.querySelector('.form__frame--finance');
+const desingFrame = document.querySelector('.form__frame--desing-think');
+const systemFrame = document.querySelector('.form__frame--system-think');
+const teamFrame = document.querySelector('.form__frame--team');
+
+const leaderBorder = document.querySelector('.record-list__elem--leader');
+const financeBorder = document.querySelector('.record-list__elem--finance');
+const desingBorder = document.querySelector('.record-list__elem--desing-think');
+const systemBorder = document.querySelector('.record-list__elem--system-think');
+const teamBorder = document.querySelector('.record-list__elem--team');
+
+const formArr = [leaderFrame, financeFrame, desingFrame, systemFrame, teamFrame];
+const borderArr = [leaderBorder, financeBorder, desingBorder, systemBorder, teamBorder]
+
+const changeElements = (arr, elem, mass, bord) => {
+  arr.forEach(element => {
+    element.style.display = 'none';
+  });
+  elem.style.display = 'block';
+  mass.forEach(element => {
+    element.style.border = 'none';
+  });
+  if (bord === leaderBorder) {
+    bord.style.border = '3px solid rgba(33, 230, 193, 0.5)';
+  } else if (bord === financeBorder) {
+    bord.style.border = '3px solid rgba(119, 90, 218, 0.5)';
+  } else if (bord === desingBorder) {
+    bord.style.border = '3px solid rgba(255, 48, 79, 0.5)';
+  } else if (bord === systemBorder) {
+    bord.style.border = '3px solid rgba(0, 38, 81, 0.5)';
+  } else if (bord === teamBorder) {
+    bord.style.border = '3px solid rgba(40, 199, 250, 0.5)';
+  }
 }
+
+leader.addEventListener('click', () => {
+  changeElements(formArr, leaderFrame, borderArr, leaderBorder);
+})
+
+finance.addEventListener('click', () => {
+  changeElements(formArr, financeFrame, borderArr, financeBorder);
+})
+
+desing.addEventListener('click', () => {
+  changeElements(formArr, desingFrame, borderArr, desingBorder);
+})
+
+system.addEventListener('click', () => {
+  changeElements(formArr, systemFrame, borderArr, systemBorder);
+})
+
+team.addEventListener('click', () => {
+  changeElements(formArr, teamFrame, borderArr, teamBorder);
+})
